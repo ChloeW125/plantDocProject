f3d64429a726fb8fdfad48cd03db3700b0638a94
ChloeW125 <139719503+ChloeW125@users.noreply.github.com>
2025-05-23 22:35:11 -0400

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,7 @@
+import streamlit
+
+def run(): 
+    streamlit.html(
+        "<h1>Welcome to PlantDoc! 🪴<h1>"
+        "<h4>Your one-stop platform for diagnosing and tracking plant health! Navigate using the sidebar to embark on your plant care journey!</h4>"
+    ) 
